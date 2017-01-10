750d81e1f8146f08bb39953f6989cdec04c59612
Jack Steffens <jack.steffens@hva.nl>
2017-01-10 21:27:40 +0100

    Concept token clearer

---
+++ utils/clearTokens.js
@@ -0,0 +1,7 @@
+exports.init = function() {
+  console.log("TODO implement. Deleting tokens");
+  // TODO Something alike
+  // Trainer.find( WHERE token != null, function(trainer) {
+  //   trainer.update( token = null )
+  // })
+}
